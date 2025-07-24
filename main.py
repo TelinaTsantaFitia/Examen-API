6cca241788e91c92036138a5cc0f816ee0f23709
TelinaTsantaFitia <hei.telina@gmail.com>
2025-07-24 09:36:13 +0300

    ajout de question3

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ def hello_world():
 @app.get("/get/welcome")
 def welcome (name : str):
     return {"message": "Hello world{name}"}
+
+# question   3
+
+class student(BaseModel):
+    Reference : str
+    FirstName : str
+    LastName : str 
+    Age : int
+
+@app.post("/student")
+def createObject(study :Study):
+    return JSONResponse(content=study)
